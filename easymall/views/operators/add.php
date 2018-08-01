6bee85984623f5dd028ab1e2f8d45624edfbbc7d
songhx@phoenix-tree.cn <BoHongtao@yeah.net>
2018-08-01 17:48:01 +0800

    一键复制

--- easymall/views/operators/add.php
+++ easymall/views/operators/add.php
@@ -2,81 +2,150 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
-$this->params['breadcrumbs'][] = ["label"=>"管理员","url"=>Url::toRoute("operators/index")];
-$this->params['breadcrumbs'][] = '增加';
 ?>
-<div class="main-content">
-    <div class="page-content">
-        <div class="page-header">
-            <h1>
-                管理员
-                <small>
-                    <i class="icon-double-angle-right"></i>
-                    增加
-                </small>
-            </h1>
-        </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <section class="panel">
-                    <div class="panel-body formBox">
-                        <?php
-                        $form = ActiveForm::begin([
-                                    'id' => 'add-operators',
-                                    'options' => [
-                                        'class' => 'form-horizontal'
-                                    ],
-                                    'fieldConfig' => [
-                                        'template' => "{label}\n<div class='col-xs-6'>{input}{error}</div><div class='col-xs-4' style='padding-left:0'>{hint}</div>",
-                                        'labelOptions' => [
-                                            'class' => 'col-xs-2 control-label'
-                                        ]
-                                    ],
-                                    'enableAjaxValidation' => true,
-                                    'validationUrl' => yii\helpers\Url::toRoute('operators/validate-add')
-                                ])
-                        ?>
-                        <?= $form->field($model, 'operator_name')->textInput(['placeholder' => '用户名'])->hint('*', ['class' => 'help-block text-danger']); ?>
-                        <?= $form->field($model, 'password')->passwordInput(['placeholder' => '密码'])->hint('*', ['class' => 'help-block text-danger']) ?>
-                        <?= $form->field($model, 're_pwd')->passwordInput(['placeholder' => '确认密码'])->hint('*', ['class' => 'help-block text-danger']) ?>
-                        <?= $form->field($model, 'role_id')->dropDownList($roles,['prompt' => '----请选择角色----'])->hint('*', ['class' => 'help-block text-danger']); ?>
-                        <?= $form->field($model, 'contact_name')->textInput(['placeholder' => '联系人']) ?>
-                        <?= $form->field($model, 'contact_phone')->textInput(['placeholder' => '联系电话']) ?>
-                        <div class="clearfix form-actions">
-                            <div class="col-md-offset-3 col-md-9">
-                                <?= Html::submitButton('确定', ['class' => 'btn btn-info', 'name' => 'login-button', 'id' => 'manager-add-btn']) ?>
-                                &nbsp; &nbsp; &nbsp;
-                                <?= Html::button('取消', ['class' => 'btn btn-default', 'style' => 'margin-left:5px', 'id' => 'manager-cancle-btn', 'onclick' => 'history.go(-1)']) ?>
-                            </div>
-                        </div>
-                        <?php ActiveForm::end(); ?>
+<link rel="stylesheet" type="text/css" href="static/css/lib/font-awesome.css" />
+<link rel="stylesheet" href="static/css/compiled/new-user.css" type="text/css" media="screen" />
+
+<div class="content">
+    <div class="container-fluid">
+        <div id="pad-wrapper" class="new-user">
+            <div class="row-fluid header">
+                <h3>添加新的管理员</h3>
+            </div>
+            <div class="row-fluid form-wrapper">
+                <!-- left column -->
+                <div class="span9 with-sidebar">
+                    <div class="container">
+                        <?php $form = ActiveForm::begin([
+                            'id' => 'add_manager_form',
+                            'options' => [
+                                'class' => 'new_user_form inline-input'
+                            ],
+                            'fieldConfig' => [
+                                'template' => "<div class='span12 field-box'>{label}{input}{error}</div> <div>{hint}</div>",
+                            ],
+                        ]); ?>
+                        <?= $form->field($model,'operator_name')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model,'password')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model, 'role_id')->dropDownList($roles,['prompt' => '----请选择角色----']) ?>
+                        <?= $form->field($model,'true_name')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model,'email')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model,'contact_phone')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model,'wechat')->textInput(['class' => 'span9']) ?>
+                        <?= $form->field($model,'head_pic')->fileInput(['class' => 'span9']) ?>
+
+                        <!--                        <div class="span12 field-box">-->
+<!--                            <label>Name:</label>-->
+<!--                            <input class="span9" type="text" />-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>State:</label>-->
+<!--                            <div class="ui-select span5">-->
+<!--                                <select>-->
+<!--                                    <option value="AK" />Alaska-->
+<!--                                    <option value="HI" />Hawaii-->
+<!--                                    <option value="CA" />California-->
+<!--                                    <option value="NV" />Nevada-->
+<!--                                    <option value="OR" />Oregon-->
+<!--                                    <option value="WA" />Washington-->
+<!--                                    <option value="AZ" />Arizona-->
+<!--                                </select>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>Company:</label>-->
+<!--                            <input class="span9" type="text" />-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>Email:</label>-->
+<!--                            <input class="span9" type="text" />-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>Phone:</label>-->
+<!--                            <input class="span9" type="text" />-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>Website:</label>-->
+<!--                            <input class="span9" type="text" />-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box">-->
+<!--                            <label>Address:</label>-->
+<!--                            <div class="address-fields">-->
+<!--                                <input class="span12" type="text" placeholder="Street address" />-->
+<!--                                <input class="span12 small" type="text" placeholder="City" />-->
+<!--                                <input class="span12 small" type="text" placeholder="State" />-->
+<!--                                <input class="span12 small last" type="text" placeholder="Postal Code" />-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="span12 field-box textarea">-->
+<!--                            <label>Notes:</label>-->
+<!--                            <textarea class="span9"></textarea>-->
+<!--                            <span class="charactersleft">90 characters remaining. Field limited to 100 characters</span>-->
+<!--                        </div>-->
+<!--                        <div class="span11 field-box actions">-->
+<!--                            <input type="button" class="btn-glow primary" value="Create user" />-->
+<!--                            <span>OR</span>-->
+<!--                            <input type="reset" value="Cancel" class="reset" />-->
+<!--                        </div>-->
+                        <?php ActiveForm::end() ?>
                     </div>
-                </section>
+                </div>
+                <!-- side right column -->
+                <div class="span3 form-sidebar pull-right">
+                    <div class="alert alert-info hidden-tablet">
+                        <i class="icon-lightbulb pull-left"></i>
+                        添加管理员请务必保证是可信赖的人员，否则可能会导致平台安全性降低
+                    </div>
+                    <h6>添加管理员注意事项:</h6>
+                    <p>尽可能使用复杂密码</p>
+                    <p>密码切勿外泄</p>
+                    <p>建议复制使用密码生成器生成密码:</p>
+                    <p>
+                        <div style="border: 2px dashed black;line-height: 20px;height: 20px;text-align:center;width: 140px;display: inline-block" id="random_password"></div>
+                        <button class="btn" data-clipboard-action="copy" data-clipboard-target="#random_password" style="margin-left: 8px;">复制</button>
+                    </p>
+                    <p onclick="generate_again()">点击重新生成</p>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<?php $this->beginBlock('script') ?>
-<script type="text/javascript">
-    $(document).on("beforeSubmit", "#add-operators", function () {
-        $('#add-operators').ajaxSubmit({
-            url: $('#add-operators').attr('action'),
-            type: 'post',
-            data: $('#add-operators').serialize(),
-            success: function (data) {
-                if (data.code == 0) {
-                    showToast('success', '添加用户成功', '2S后返回', 5500);
-                    setTimeout('window.location.href="<?= Url::toRoute(['operators/index']) ?>"', 1500);
-                } else {
-                    showToast('error', '添加用户失败', data.desc, 3000);
-                    $('#manager-add-btn').html('确定');
-                    $('#manager-add-btn').attr('disabled', false);
-                    $('#manager-cancle-btn').attr('disabled', false);
-                }
+
+<?php $this->beginBlock('script'); ?>
+    <script src="static/js/clipboard.min.js"></script>
+    <script>
+        /*
+         *  随机产生密码
+         *  @param   size   int  生成密码的长度
+         */
+        function randomPassword(size) {
+            var seed = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z', 'a','b','c','d','e','f','g','h','i','j','k','m','n','p','Q','r','s','t','u','v','w','x','y','z', '2','3','4','5','6','7','8','9','$','!','@','#','*');
+            seedlenth = seed.length;
+            var createPassword = '';
+            for (i = 0 ; i < size; ++ i){
+                j = Math.floor(Math.random()*seedlenth);
+                createPassword += seed[j];
             }
+            return createPassword;
+        }
+        /*
+         *  改变div里面的密码
+         */
+        function generate_again() {
+            $('#random_password').text(randomPassword(8))
+        }
+        $(function () {
+            generate_again()
+        })
+        /*
+         *  一键复制密码
+         */
+        var clipboard = new ClipboardJS('.btn');
+        clipboard.on('success', function(e) {
+            console.log(e);
         });
-        return false; // Cancel form submitting.
-    });
-</script>
-<?php
-$this->endBlock()?>
+        clipboard.on('error', function(e) {
+            console.log(e);
+        });
+    </script>
+<?php $this->endBlock(); ?>
