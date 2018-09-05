908f6ca5682715ae1f2b3666e7aae7e1c27b320c
songhx@phoenix-tree.cn <BoHongtao@yeah.net>
2018-09-05 20:55:21 +0800

    user

---
+++ easymall/controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 2018/9/5
+ * Time: 20:37
+ */
+namespace app\controllers;
+use Yii;
+
+class UserController extends BaseController
+{
+    /*
+     * 首页
+     */
+    public function actionIndex()
+    {
+
+    }
+    /*
+     * 用户列表
+     */
+    public function actionData()
+    {
+
+    }
+}
